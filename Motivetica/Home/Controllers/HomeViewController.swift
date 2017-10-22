696b337933b91e67d792367e9eb0fb90e37e78e0
minikin <djminikin@gmail.com>
2017-10-22 21:53:24 +0200

    Add swiftlint

---
+++ Motivetica/Home/Controllers/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  Motivetica
+//
+//  Created by Sasha Prokhorenko on 10/22/17.
+//  Copyright © 2017 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Motivetica/UserQuote/ViewController/UserQuoteViewController.swift
@@ -0,0 +1,35 @@
+/**
+* UserQuoteViewController.swift
+*
+* Motivetica
+* Created by: Sasha Prokhorenko on 10/22/17
+* Copyright (c) 2017 Sasha Prokhorenko
+*/
+
+import UIKit
+
+class UserQuoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
