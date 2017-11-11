efe8c1ae9031a3c9e5a74a3f47a3a7bc1c757edb
minikin <djminikin@gmail.com>
2017-11-11 17:48:58 +0100

    Add QuotesListLayout

---
+++ Motivetica/QuotesList/Views/QuotesListLayout.swift
@@ -0,0 +1,63 @@
+//
+//  QuotesListLayout.swift
+//  Motivetica
+//
+//  Created by Sasha Prokhorenko on 11/11/17.
+//  Copyright © 2017 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+final class QuotesListLayout: UICollectionViewFlowLayout {
+  
+  override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint,
+                                    withScrollingVelocity velocity: CGPoint) -> CGPoint {
+    
+    if let cv = self.collectionView {
+      
+      let cvBounds = cv.bounds
+      let halfWidth = cvBounds.size.width * 0.5;
+      
+      let proposedContentOffsetCenterX = proposedContentOffset.x + halfWidth;
+      
+      if let attributesForVisibleCells = self.layoutAttributesForElements(in: cvBounds) {
+        
+        var candidateAttributes : UICollectionViewLayoutAttributes?
+        for attributes in attributesForVisibleCells {
+          
+          // == Skip comparison with non-cell items (headers and footers) == //
+          if attributes.representedElementCategory != UICollectionElementCategory.cell {
+            continue
+          }
+          
+          if (attributes.center.x == 0) || (attributes.center.x > (cv.contentOffset.x + halfWidth) && velocity.x < 0) {
+            continue
+          }
+          
+          // == First time in the loop == //
+          guard let candAttrs = candidateAttributes else {
+            candidateAttributes = attributes
+            continue
+          }
+          
+          let a = attributes.center.x - proposedContentOffsetCenterX
+          let b = candAttrs.center.x - proposedContentOffsetCenterX
+          
+          if fabsf(Float(a)) < fabsf(Float(b)) {
+            candidateAttributes = attributes;
+          }
+        }
+        
+        // Beautification step
+        if(proposedContentOffset.x == -(cv.contentInset.left)) {
+          return proposedContentOffset
+        }
+        return CGPoint(x: floor(candidateAttributes!.center.x - halfWidth), y: proposedContentOffset.y)
+      }
+    }
+    
+    // fallback
+    return super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
+  }
+  
+}
